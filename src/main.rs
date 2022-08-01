1edb40bc2f0971de447874f17fb5cbaaab56c1aa
Max Koverdyaev <shadeglare@gmail.com>
2022-08-01 17:56:04 +0300

    [init]

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(ping))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
