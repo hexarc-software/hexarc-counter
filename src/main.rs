ebb01f6049093ea16bfd370b5b529718b5935ccb
Max Koverdyaev <shadeglare@gmail.com>
2022-08-02 10:25:57 +0300

    [feature] added using of env variables

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder, web};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
 
 mod model;
 use model::Point;
@@ -16,8 +17,13 @@ async fn point() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port: u16 = env::var("PORT")
+        .expect("PORT expected")
+        .parse()
+        .expect("PORT must be a number");
+
     HttpServer::new(|| App::new().service(ping).service(point))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", port))?
         .run()
         .await
 }
